8c78fbac62cf996badfee38dd4b7b69e3fadcf37
Francisco Rossi <50388097+frneer@users.noreply.github.com>
2025-02-05 11:52:28 -0300

    Add utilities to quantize any existing model with any quantizer (#11)
    This patch adds utility functions to allow full control over the
    quantization process of any Keras model.

---
+++ src/configs/generate_config.py
@@ -0,0 +1,100 @@
+"""GenerateConfig This module contains an automatic generation of a
+quantization configuration for a Keras model."""
+
+from typing import Tuple
+
+from tensorflow import Tensor
+from tensorflow.keras.layers import Layer
+from tensorflow_model_optimization.python.core.quantization.keras.quantize_config import (
+    QuantizeConfig,
+)
+from tensorflow_model_optimization.python.core.quantization.keras.quantizers import (
+    Quantizer,
+)
+
+AttributeQuantizerDict = dict[str, Quantizer | dict[str, Quantizer]]
+
+
+def get_nested_attribute(obj: object, attribute: str) -> object:
+    """Get a nested attribute from an object."""
+    attributes = attribute.split(".")
+    for attr in attributes:
+        obj = getattr(obj, attr)
+    return obj
+
+
+def set_nested_attribute(obj: object, attribute: str, value: object):
+    """Set a nested attribute from an object."""
+    attributes = attribute.split(".")
+    for attr in attributes[:-1]:
+        obj = getattr(obj, attr)
+    setattr(obj, attributes[-1], value)
+
+
+def flatten_nested_dict(nested_dict: dict, prefix: str = "") -> dict:
+    """Parse a nested dictionary into a flat dictionary."""
+    flat_dict = {}
+    for key, value in nested_dict.items():
+        if isinstance(value, dict):
+            flat_dict.update(flatten_nested_dict(value, f"{prefix}{key}."))
+        else:
+            flat_dict[f"{prefix}{key}"] = value
+    return flat_dict
+
+
+class GenerateConfig(QuantizeConfig):
+    """Generate a quantization configuration for a Keras model."""
+
+    def __init__(
+        self,
+        weights: AttributeQuantizerDict = {},
+        activations: AttributeQuantizerDict = {},
+    ):
+        self.weights = flatten_nested_dict(weights)
+        self.activations = flatten_nested_dict(activations)
+
+    def get_weights_and_quantizers(
+        self, layer: Layer
+    ) -> list[Tuple[Tensor, Quantizer]]:
+        weights_and_quantizers = []
+        for weight_attr, quantizer in self.weights.items():
+            weights_and_quantizers.append(
+                (get_nested_attribute(layer, weight_attr), quantizer)
+            )
+        return weights_and_quantizers
+
+    def set_quantize_weights(
+        self, layer: Layer, quantize_weights: list[Tensor]
+    ):
+        for attribute, quantized_weight in zip(
+            self.weights.keys(), quantize_weights
+        ):
+            set_nested_attribute(layer, attribute, quantized_weight)
+
+    def get_activations_and_quantizers(
+        self, layer: Layer
+    ) -> list[Tuple[Tensor, Quantizer]]:
+        activations_and_quantizers = []
+        for activation_attribute, quantizer in self.activations.items():
+            activations_and_quantizers.append(
+                (get_nested_attribute(layer, activation_attribute), quantizer)
+            )
+        return activations_and_quantizers
+
+    def set_quantize_activations(
+        self, layer: Layer, quantize_activations: list[Tensor]
+    ):
+        for attribute, quantized_activation in zip(
+            self.activations.keys(), quantize_activations
+        ):
+            set_nested_attribute(layer, attribute, quantized_activation)
+
+    def get_output_quantizers(self, layer):
+        return []
+
+    def get_config(self):
+        return {"weights": self.weights, "activations": self.activations}
+
+    @classmethod
+    def from_config(cls, config):
+        return cls(**config)
---
+++ src/configs/qmodel.py
@@ -0,0 +1,114 @@
+"""QModel.
+
+This module contains the utilities to quantize a Keras model.
+"""
+
+from configs.generate_config import AttributeQuantizerDict, GenerateConfig
+from tensorflow.keras.initializers import Constant
+from tensorflow.keras.layers import Layer
+from tensorflow.keras.models import Model, clone_model
+from tensorflow_model_optimization.quantization.keras import (
+    quantize_annotate_layer,
+    quantize_apply,
+    quantize_scope,
+)
+
+from quantizers.uniform_quantizer import UniformQuantizer
+
+# {layer_name: AttributeQuantizerDict}
+LayerQuantizerDict = dict[str, AttributeQuantizerDict]
+
+
+def clone_layer(layer: Layer):
+    """Clone a Keras layer.
+
+    It also copies the weights from the original layer to the clone.
+    """
+    clone = layer.__class__.from_config(layer.get_config())
+
+    # If the layer has an input shape, build it so we can copy the weights
+    if hasattr(layer, "input_shape"):
+        clone.build(layer.input_shape)
+
+    clone.set_weights(layer.get_weights())
+
+    return clone
+
+
+def quantize_layer(layer: Layer, layer_quantizers: AttributeQuantizerDict):
+    """Quantize a Keras layer.
+    Annotates a cloned layer with the quantizers specified in the layer_quantizers dict.
+
+    param(layer): Keras layer to quantize.
+    param(layer_quantizers): Dictionary with the quantizers to apply to the layer.
+
+    Example:
+
+    ```python
+    quantize_layer(
+        Dense(128, activation="relu", name="hidden"),
+        {
+            "weights": {"kernel": UniformQuantizer(8), "bias": UniformQuantizer(8)},
+            "activations": {"activation": UniformQuantizer(8)},
+        },
+    )
+    ```
+
+    """
+    return quantize_annotate_layer(
+        clone_layer(layer), GenerateConfig(**layer_quantizers)
+    )
+
+
+def quantize_model(model: Model, quantizers: LayerQuantizerDict):
+    """Quantize a Keras model.
+
+    param(model): Keras model to quantize.
+    param(quantizers): Dictionary with the quantizers to apply to the model.
+
+    """
+    def clone_function(layer: Layer):
+        if layer.name in quantizers:
+            return quantize_layer(layer, quantizers[layer.name])
+        else:
+            return clone_layer(layer)
+
+    return clone_model(
+        model, clone_function=lambda layer: clone_function(layer)
+    )
+
+
+def apply_quantization(model: Model, quantizers: LayerQuantizerDict):
+    """Apply quantization to a Keras model.
+
+    param(model): Keras model to quantize.
+    param(quantizers): Dictionary with the quantizers to apply to the model.
+
+    Example:
+    ```python
+    layer_2 = Dense(128, activation="relu", name="hidden")
+    apply_quantization(
+        Sequential([layer_1, layer_2, layer_3]),
+        {
+            "hidden": {
+                "weights": {"kernel": UniformQuantizer(8), "bias": UniformQuantizer(8)},
+                "activations": {"activation": UniformQuantizer(8)},
+            }
+        },
+    )
+    ```
+    """
+
+    model_layers = [layer.name for layer in model.layers]
+    for layer_name in quantizers.keys():
+        if layer_name not in model_layers:
+            raise ValueError(f"Layer {layer_name} not found in model {model.name}.")
+    # TODO(Fran): get below dict objects from the quantizers passed in add method (Constant comes from UniformQuantizer)
+    # So maybe if there are custom objects to register each class should have a method to return them
+    custom_objects = {}
+    custom_objects["GenerateConfig"] = GenerateConfig
+    custom_objects["UniformQuantizer"] = UniformQuantizer
+    custom_objects["Constant"] = Constant
+
+    with quantize_scope(custom_objects):
+        return quantize_apply(quantize_model(model, quantizers))
