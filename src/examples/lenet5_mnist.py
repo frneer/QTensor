2c140424049c1bacb66bba5c6b56f8e09c6ac1ac
colorete87 <colorete87@gmail.com>
2025-04-02 03:11:53 -0300

    test: lenet and mnist dataset, with unirform and flex q ready

--- src/examples/lenet5_mnist.py
+++ src/examples/lenet5_mnist.py
@@ -22,29 +22,37 @@ from pathlib import Path
 from quantizers.flex_quantizer import FlexQuantizer
 from quantizers.uniform_quantizer import UniformQuantizer
 
-from batchnorm_folding import apply_bn_folding
+from functions import apply_bn_folding, compute_alpha_dict, apply_alpha_dict
 
 
 
 qconfig_uniform = {
-        "conv2d"              : { "weights": {"kernel": UniformQuantizer(bits=8 , signed=True)}, "activations": {"activation": UniformQuantizer(bits=8 , signed=False)}, },
-        #"average_pooling2d"   : { "weights": {"kernel": UniformQuantizer(bits=8 , signed=True)}, "activations": {"activation": UniformQuantizer(bits=8 , signed=False)}, },
-        "conv2d_1"            : { "weights": {"kernel": UniformQuantizer(bits=8 , signed=True)}, "activations": {"activation": UniformQuantizer(bits=8 , signed=False)}, },
-        #"average_pooling2d_1" : { "weights": {"kernel": UniformQuantizer(bits=8 , signed=True)}, "activations": {"activation": UniformQuantizer(bits=8 , signed=False)}, },
-        #"flatten"             : { "weights": {"kernel": UniformQuantizer(bits=8 , signed=True)}, "activations": {"activation": UniformQuantizer(bits=8 , signed=False)}, },
-        "dense"               : { "weights": {"kernel": UniformQuantizer(bits=8 , signed=True)}, "activations": {"activation": UniformQuantizer(bits=8 , signed=False)}, },
-        "dense_1"             : { "weights": {"kernel": UniformQuantizer(bits=8 , signed=True)}, "activations": {"activation": UniformQuantizer(bits=8 , signed=False)}, },
-        #"dense_2"             : { "weights": {"kernel": UniformQuantizer(bits=8 , signed=True)}, "activations": {"activation": UniformQuantizer(bits=8 , signed=False)}, },
+        "conv2d"    : { "weights": {"kernel": UniformQuantizer(bits=8, signed=True)}, "activations": {"activation": UniformQuantizer(bits=8 , signed=False)}, },
+        "conv2d_1"  : { "weights": {"kernel": UniformQuantizer(bits=8, signed=True)}, "activations": {"activation": UniformQuantizer(bits=8 , signed=False)}, },
+        "dense"     : { "weights": {"kernel": UniformQuantizer(bits=8, signed=True)}, "activations": {"activation": UniformQuantizer(bits=8 , signed=False)}, },
+        "dense_1"   : { "weights": {"kernel": UniformQuantizer(bits=8, signed=True)}, "activations": {"activation": UniformQuantizer(bits=8 , signed=False)}, },
+        #"dense_2"   : { "weights": {"kernel": UniformQuantizer(bits=8, signed=True)}, "activations": {"activation": UniformQuantizer(bits=8 , signed=False)}, },
         }
-qconfig = qconfig_uniform
+qconfig_flex = {
+        "conv2d"    : { "weights": {"kernel": FlexQuantizer(bits=8, n_levels=2, signed=True)}, "activations": {"activation": UniformQuantizer(bits=8 , signed=False)}, },
+        "conv2d_1"  : { "weights": {"kernel": FlexQuantizer(bits=8, n_levels=2, signed=True)}, "activations": {"activation": UniformQuantizer(bits=8 , signed=False)}, },
+        "dense"     : { "weights": {"kernel": FlexQuantizer(bits=8, n_levels=2, signed=True)}, "activations": {"activation": UniformQuantizer(bits=8 , signed=False)}, },
+        "dense_1"   : { "weights": {"kernel": FlexQuantizer(bits=8, n_levels=2, signed=True)}, "activations": {"activation": UniformQuantizer(bits=8 , signed=False)}, },
+        #"dense_2"   : { "weights": {"kernel": FlexQuantizer(bits=8, n_levels=2, signed=True)}, "activations": {"activation": UniformQuantizer(bits=8 , signed=False)}, },
+        }
+qconfig = qconfig_flex
 
 output_path = Path("snapshots")
+
+# Pre-training parameters
 pre_training_learning_rate = 0.001
-pre_training_epochs = 1
-pre_training_batch_size = 128
-learning_rate = 0.001
-epochs = 1
-batch_size = 128
+pre_training_epochs = 10
+pre_training_batch_size = 32
+
+# QAT parameters
+learning_rate = 0.0001
+epochs = 10
+batch_size = 32
 
 if __name__ == "__main__":
 
@@ -57,19 +65,21 @@ if __name__ == "__main__":
     y_train = to_categorical(y_train, 10)
     y_test = to_categorical(y_test, 10)
 
-    input_shape=(28, 28, 1)
+    input_shape = x_train.shape[1:]
 
     model = models.Sequential()
-    model.add(layers.Conv2D(6, kernel_size=5, activation='tanh', input_shape=input_shape, padding='same'))
+    model.add(layers.Conv2D(6, kernel_size=5, activation='relu', input_shape=input_shape, padding='same'))
     model.add(layers.AveragePooling2D())
-    model.add(layers.Conv2D(16, kernel_size=5, activation='tanh'))
+    model.add(layers.Conv2D(16, kernel_size=5, activation='relu'))
     model.add(layers.AveragePooling2D())
     model.add(layers.Flatten())
-    model.add(layers.Dense(120, activation='tanh'))
-    model.add(layers.Dense(84, activation='tanh'))
+    model.add(layers.Dense(120, activation='relu'))
+    model.add(layers.Dense(84, activation='relu'))
     model.add(layers.Dense(10, activation='softmax'))
 
+    print(f"#####################################################")
     model.summary(line_length=100)
+    print(f"#####################################################\n")
 
     model.compile(
         optimizer=Adam(learning_rate=pre_training_learning_rate),
@@ -77,15 +87,19 @@ if __name__ == "__main__":
         metrics=['accuracy']
     )
 
-    model.fit(x_train, y_train,
-              batch_size=batch_size,
-              epochs=pre_training_epochs,
-              validation_split=0.1
-              )
+    print(f"#####################################################")
+    print(f"Pre-Training")
+    if pre_training_epochs > 0:
+        model.fit(x_train, y_train,
+                  batch_size=pre_training_batch_size,
+                  epochs=pre_training_epochs,
+                  validation_split=0.1
+                  )
+    print(f"#####################################################\n")
 
     print(f"#####################################################")
+    print(f"POST FIT Evaluation")
     loss, accuracy = model.evaluate(x=x_test, y=y_test)
-    print(f"POST FIT Evaluation: loss={loss}, accuracy={accuracy}")
     print(f"#####################################################\n")
 
     fmodel = apply_bn_folding(model, False)
@@ -95,49 +109,18 @@ if __name__ == "__main__":
         loss="categorical_crossentropy",
         metrics=["accuracy"],
     )
-    #fmodel.fit(
-    #    train_dataset,
-    #    epochs=args.pre_training_epochs,
-    #    #batch_size=args.pre_training_batch_size,
-    #    validation_data=validation_dataset
-    #)
-
-    # Code to define the initial alpha values for weights
-    for i,l in enumerate(fmodel.layers):
-        for j,v in enumerate(l.weights):
-            print(f"{i:04d}: (max,min)=({np.max(v)},{np.min(v)}) {j+1}/{len(l.weights)} ({v.shape}) ({l.name})")
-
-
-    ## Code to define the initial alpha values for activation
-    ## After training, create a new model that outputs the activations of all layers.
-    #min_val = +np.inf * np.ones(len(fmodel.layers))
-    #max_val = -np.inf * np.ones(len(fmodel.layers))
-    #for j, input_ in enumerate(x_train):
-    #    print(f"{j:04d}/{len(x_train)}")
-    #    input_ = input_[None,...]
-    #    for i,layer in enumerate(fmodel.layers):
-    #        output = layer(input_, training=False)
-
-    #        min_aux = np.min(output)
-    #        if min_aux < min_val[i]:
-    #            min_val[i] = min_aux
-
-    #        max_aux = np.max(output)
-    #        if max_aux < max_val[i]:
-    #            max_val[i] = max_aux
-
-    #        input_ = output
-
-    #for i, (min_, max_, layer) in enumerate(zip(min_val, max_val, fmodel.layers)):
-    #    print(f"{i:04d}: (max,min)=(min_, max_) ({layer.name})")
-
 
     print(f"#####################################################")
+    print(f"POST BN FOLDING Evaluation")
     loss, accuracy = fmodel.evaluate(x=x_test, y=y_test)
-    print(f"POST BN FOLDING Evaluation: loss={loss}, accuracy={accuracy}")
     print(f"#####################################################\n")
 
     # Apply quantization
+    print(f"#####################################################")
+    print(f"QConfig")
+    for i, qc in enumerate(qconfig.values()):
+        print(f"{i:04d}: {qc}")
+    print(f"#####################################################\n")
     qmodel = apply_quantization(fmodel, qconfig)
     qmodel.build(input_shape=input_shape)
     qmodel.compile(
@@ -146,15 +129,35 @@ if __name__ == "__main__":
         metrics=["accuracy"],
     )
 
+
+    alpha_dict = compute_alpha_dict(fmodel, x_train)
+
+
+    # ----------------------------
+    # Print the complete alpha_dict with weights and activation alpha values.
+    print("#####################################################")
+    print("Alpha Dictionary (Weights and Activations)")
+    for i, (layer_name, weights_dict) in enumerate(alpha_dict.items()):
+        print(f"{i:04d}: {layer_name}")
+        for key, alpha_value in weights_dict.items():
+            print(f"    {key}: {alpha_value}")
+    print("#####################################################\n")
+    
+
+    qmodel = apply_alpha_dict(qmodel, alpha_dict)
+    
+
+
     print(f"#####################################################")
+    print(f"POST QUANTIZATION Evaluation")
     loss, accuracy = qmodel.evaluate(x=x_test, y=y_test)
-    print(f"POST QUANTIZATION Evaluation: loss={loss}, accuracy={accuracy}")
     print(f"#####################################################\n")
 
 
     callback_tuples = [(CaptureWeightCallback(qlayer), qconfig[layer.name]) for layer, qlayer in zip(model.layers, qmodel.layers) if layer.name in qconfig]
 
     print(f"#####################################################")
+    print(f"QAT")
     if epochs > 0:
         hist = qmodel.fit(
             x_train,
@@ -167,8 +170,8 @@ if __name__ == "__main__":
     print(f"#####################################################\n")
 
     print(f"#####################################################")
+    print(f"POST QAT Evaluation")
     loss, accuracy = qmodel.evaluate(x=x_test, y=y_test)
-    print(f"POST QAT Evaluation: loss={loss}, accuracy={accuracy}")
     print(f"#####################################################\n")
 
     #model.summary(line_length=100)
@@ -183,6 +186,5 @@ if __name__ == "__main__":
     with open(output_path / "output_dict.pkl", "wb") as f:
         pickle.dump(output_dict, f)
 
-    #qmodel.evaluate(test_dataset)
 
 
