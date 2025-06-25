ce49c83fbd2be35888181465b60549219763b47f
colorete87 <colorete87@gmail.com>
2025-06-25 07:26:54 -0300

    Discarded 2 quantization stages, for now. Triying with 16 bits for A

--- src/stage/main.py
+++ src/stage/main.py
@@ -57,10 +57,10 @@ stages_hyperparams = [
                 {"type": "uniform", "bits": 8},
             ],
             "activations": [
-                {"type": None},
-                {"type": None},
-                {"type": None},
-                {"type": None},
+                {"type": "uniform", "bits": 16},
+                {"type": "uniform", "bits": 16},
+                {"type": "uniform", "bits": 16},
+                {"type": "uniform", "bits": 16},
             ],
         },
     },
@@ -91,22 +91,7 @@ stages_hyperparams = [
             "validation_split": 0.1,
         },
     },
-    # Stage 5: Model quantization
-    {
-        "name": "quantization",
-        "seed": 12345,
-        "function": "model_quantize",
-        "kwargs": {
-            "input_shape": [None, 32, 32, 3],
-            "activations": [
-                {"type": "uniform", "bits": 8},
-                {"type": "uniform", "bits": 8},
-                {"type": "uniform", "bits": 8},
-                {"type": "uniform", "bits": 8},
-            ],
-        },
-    },
-    # Stage 6: Final Evaluation
+    # Stage 5: Final Evaluation
     {
         "name": "final_evaluation",
         "seed": 12345,
