219d26bac059f52d2e3f4a6afe859641480d6cbb
Francisco Rossi <frrossi@fi.uba.ar>
2025-10-11 15:39:31 -0300

    More work

--- src/plot_scripts/quantizer_plot.py
+++ src/plot_scripts/quantizer_plot.py
@@ -14,12 +14,21 @@ def plot_levels(ax, thresholds, levels, **kwargs):
         linestyle=kwargs.get("linestyle", "-"),
         lw=1,
         where="post",
+        label=kwargs.get("label", "función de cuantización"),
     )
 
 
 def plot_ste(ax, min_limit, max_limit):
     dq_dx = np.linspace(min_limit, max_limit, 100)
-    ax.plot(dq_dx, dq_dx, color="purple", linestyle="-", lw=2, alpha=0.5)
+    ax.plot(
+        dq_dx,
+        dq_dx,
+        color="purple",
+        linestyle="-.",
+        lw=1,
+        alpha=0.5,
+        label="ste",
+    )
 
 
 def plot_limits(ax, min_limit, max_limit):
@@ -68,7 +77,9 @@ def generate_non_uniform(alpha, signed, m_levels):
     return thresholds, levels
 
 
-def plot_uniform(bits, alpha, signed, fig_path, plot_ste=False):
+def plot_uniform(
+    bits, alpha, signed, fig_path, do_plot_ste=False, alpha_generic=False
+):
     fig, ax = plt.subplots()
 
     # Generate quantization levels and thresholds
@@ -80,42 +91,51 @@ def plot_uniform(bits, alpha, signed, fig_path, plot_ste=False):
     # Plots
     plot_limits(ax, min_limit, max_limit)
     plot_levels(ax, thresholds, levels)
-    if plot_ste:
+    if do_plot_ste:
         plot_ste(ax, min_limit, max_limit)
 
-    plt.xticks(
-        ticks=thresholds, labels=[f"{t:.3f}" for t in thresholds], rotation=45
-    )
-    plt.yticks(
-        ticks=levels[:-1],
-        labels=[f"{l:.3f}" for l in levels[:-1]],
-        rotation=45,
-    )
-    # # alpha annotated ticks
-    # step = 2 * delta(alpha, m_levels, signed) # double de step to generate major/minor ticks
-    # ## Set major ticks
-    # labels = [
-    #     r"$-\alpha$",
-    #     r"$-\frac{\alpha}{2}$",
-    #     "0",
-    #     r"$\frac{\alpha}{2}$",
-    #     r"$\alpha$",
-    # ]
-    # plt.xticks(ticks=np.arange(min_limit, max_limit + step, step), labels=labels)
-    # plt.yticks(ticks=np.arange(min_limit, max_limit + step, step), labels=labels)
-    # ## Set minor ticks
-    # ax = plt.gca()
-    # ax.set_xticks(np.arange(min_limit + step, max_limit, step), minor=True)
-    # ax.set_yticks(np.arange(min_limit + step, max_limit, step), minor=True)
+    thresholds_ticks = thresholds
+    levels_ticks = levels  # Exclude the last level for ticks
+    levels_ticks[-1] = max_limit  # Ensure the last tick is at max_limit
+    thresholds_labels = [f"{t:.3f}" for t in thresholds]
+    levels_labels = [f"{l:.3f}" for l in levels]
+    rotation = 45
 
+    if alpha_generic and bits == 3 and signed:
+        m_levels = 2**bits
+        # double de step to generate major/minor ticks
+        step = delta(alpha, m_levels, signed)
+
+        levels_ticks = np.arange(min_limit, max_limit + step, step)
+        # Set major ticks
+        labels = [
+            r"$-\alpha$",
+            "",
+            r"$-\frac{\alpha}{2}$",
+            "",
+            "0",
+            "",
+            r"$\frac{\alpha}{2}$",
+            "",
+            r"$\alpha$",
+        ]
+        thresholds_labels = labels
+        levels_labels = labels
+        rotation = 0
+
+    plt.xticks(
+        ticks=thresholds_ticks, labels=thresholds_labels, rotation=rotation
+    )
+    plt.yticks(ticks=levels_ticks, labels=levels_labels, rotation=rotation)
     # Annotations
     ax.set_xlabel(r"$x$")
-    ax.set_ylabel(r"$q(x; \alpha)$")
+    ax.set_ylabel(r"$q$")
     ax.grid(which="both", alpha=0.3, linestyle=":")
+    ax.legend(loc="upper left", fontsize=10)
     plt.savefig(f"{fig_path}.png")
 
 
-def plot_sym_non_sym_uniform(bits, alpha, signed, fig_path, plot_ste=False):
+def plot_sym_non_sym_uniform(bits, alpha, signed, fig_path, do_plot_ste=False):
     fig, ax = plt.subplots()
 
     thresholds, levels = generate_uniform(bits, alpha, signed)
@@ -133,12 +153,14 @@ def plot_sym_non_sym_uniform(bits, alpha, signed, fig_path, plot_ste=False):
 
     # Annotations
     ax.set_xlabel(r"$x$")
-    ax.set_ylabel(r"$q(x; \alpha)$")
+    ax.set_ylabel(r"$q$")
     # ax.grid(which="both", alpha=0.3, linestyle=":")
     plt.savefig(f"{fig_path}.png")
 
 
-def plot_uniform_vs_non_uniform(bits, alpha, signed, fig_path, plot_ste=False):
+def plot_uniform_vs_non_uniform(
+    bits, alpha, signed, fig_path, do_plot_ste=False
+):
     fig, ax = plt.subplots()
 
     thresholds, levels = generate_uniform(bits, alpha, signed)
@@ -156,8 +178,124 @@ def plot_uniform_vs_non_uniform(bits, alpha, signed, fig_path, plot_ste=False):
 
     # Annotations
     ax.set_xlabel(r"$x$")
-    ax.set_ylabel(r"$q(x; \alpha)$")
+    ax.set_ylabel(r"$q$")
     # ax.grid(which="both", alpha=0.3, linestyle=":")
+    plt.savefig(f"{fig_path}.png")
+
+
+def generate_flex_quantizer_fixed(bits, alpha, signed):
+    # Data taken from training a model with flex quantization
+    pass
+    # levels += [levels[-1]]
+    qlevels = [-0.15165123, -0.11373842, -0.03791281, 0.0, 0.07582562]
+    qlevels = qlevels + [qlevels[-1]]
+    # qlevels += [qlevels[-1]]
+    thresholds = [
+        -0.15165123,
+        -0.01363241,
+        0.0057151,
+        0.0173785,
+        0.08350082,
+        0.15165123,
+    ]
+
+    return thresholds, qlevels
+
+
+def generate_non_uniform_fixed():
+    levels = [-0.15165123, -0.08514632, -0.02999737, 0.02491043, 0.07670338]
+    levels = levels + [levels[-1]]
+    thresholds = [
+        -0.15165123,
+        -0.01363241,
+        0.0057151,
+        0.0173785,
+        0.08350082,
+        0.15165123,
+    ]
+
+    return thresholds, levels
+
+
+def plot_flex_quantizer(fig_path, do_plot_prequantized, do_plot_ste=False):
+    thresholds, levels = generate_flex_quantizer_fixed(
+        bits=3, alpha=1, signed=True
+    )
+    thresholds_nu, levels_nu = generate_non_uniform_fixed()
+
+    fig, ax = plt.subplots()
+
+    plot_limits(ax, thresholds[0], thresholds[-1])
+
+    plot_levels(
+        ax,
+        thresholds,
+        levels,
+        color="blue",
+        linestyle="-",
+        label="función de cuantización",
+    )
+
+    if do_plot_ste:
+        plot_ste(ax, thresholds[0], thresholds[-1])
+
+    if do_plot_prequantized:
+        plot_levels(
+            ax,
+            thresholds_nu,
+            levels_nu,
+            color="orange",
+            linestyle=":",
+            label="función con niveles sin cuantizar",
+        )
+
+    ax.legend(loc="upper left", fontsize=10)
+    # Annotations
+    ax.set_xlabel(r"$x$")
+    ax.set_ylabel(r"$q$")
+    ax.grid(which="both", alpha=0.3, linestyle=":")
+    ax.legend(loc="upper left", fontsize=10)
+
+    x_labels = [rf"$t_0 = -\alpha$"]
+    x_labels += [rf"$t_{i}$" for i in range(1, len(thresholds) - 1)]
+    x_labels += [rf"$t_{len(thresholds) - 1} = \alpha$"]
+
+    plt.xticks(thresholds, labels=x_labels)
+    plt.yticks(levels, labels=[rf"$l_{i}$" for i in range(len(levels))])
+
+    plt.savefig(f"{fig_path}.png")
+
+
+def plot_non_uniform_quantizer(fig_path):
+    thresholds, levels = generate_non_uniform_fixed()
+
+    fig, ax = plt.subplots()
+
+    plot_limits(ax, thresholds[0], thresholds[-1])
+
+    plot_levels(
+        ax,
+        thresholds,
+        levels,
+        color="blue",
+        linestyle="-",
+        label="función de cuantización",
+    )
+
+    ax.legend(loc="upper left", fontsize=10)
+    # Annotations
+    ax.set_xlabel(r"$x$")
+    ax.set_ylabel(r"$q$")
+    ax.grid(which="both", alpha=0.3, linestyle=":")
+    ax.legend(loc="upper left", fontsize=10)
+
+    x_labels = [rf"$t_0 = -\alpha$"]
+    x_labels += [rf"$t_{i}$" for i in range(1, len(thresholds) - 1)]
+    x_labels += [rf"$t_{len(thresholds) - 1} = \alpha$"]
+
+    plt.xticks(thresholds, labels=x_labels)
+    plt.yticks(levels, labels=[rf"$l_{i}$" for i in range(len(levels))])
+
     plt.savefig(f"{fig_path}.png")
 
 
@@ -171,3 +309,41 @@ if __name__ == "__main__":
         bits=3, alpha=1, signed=True, fig_path="uniform_vs_non_uniform_3bits"
     )
     plot_uniform(bits=3, alpha=4, signed=True, fig_path="signed_3bits_alpha4")
+    plot_uniform(
+        bits=3,
+        alpha=1,
+        signed=True,
+        fig_path="signed_generic_3bits",
+        alpha_generic=True,
+    )
+    plot_uniform(
+        bits=3,
+        alpha=1,
+        signed=True,
+        fig_path="signed_generic_3bits_ste",
+        alpha_generic=True,
+        do_plot_ste=True,
+    )
+
+    plot_flex_quantizer(
+        fig_path="flex_quantizer_3bits",
+        do_plot_prequantized=True,
+        do_plot_ste=True,
+    )
+    plot_flex_quantizer(
+        fig_path="flex_quantizer_3bits_no_prequantized",
+        do_plot_prequantized=False,
+        do_plot_ste=True,
+    )
+    plot_flex_quantizer(
+        fig_path="flex_quantizer_3bits_no_prequantized_no_ste",
+        do_plot_prequantized=False,
+        do_plot_ste=False,
+    )
+    plot_flex_quantizer(
+        fig_path="flex_quantizer_3bits_no_ste",
+        do_plot_prequantized=True,
+        do_plot_ste=False,
+    )
+
+    plot_non_uniform_quantizer(fig_path="non_uniform_quantizer_3bits")
